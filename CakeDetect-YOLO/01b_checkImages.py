359d10cdaac4b3c8e724574bfd9b555372f87a7b
Luna <bluna@berkeley.edu>
2020-01-22 08:17:08 -0800

    Added file to check for image dimension and annotation files

--- CakeDetect-YOLO/01b_checkImages.py
+++ CakeDetect-YOLO/01b_checkImages.py
@@ -4,9 +4,15 @@
 #
 # Description:
 # ------------
-# Script to check you can open the image without fail, 
-# checks it has proper height and width and hieght. 
+# Script to check:
+# + Image opens without fail (i.e. not corrupted)
+# + Image has proper height and width and hieght. 
+# + Each .jpg image has an associated annotated bounded box .xml file
 #
+# Returns
+# -------
+# A missingAnnotations.txt file containing the name of .jpg which
+# do not have bounding box annotations .xml file.
 # -------------------------------------------------------------------
 
 import os
@@ -36,7 +42,7 @@ def checkImageCanOpen(imageFile:str):
 		# Remove image 
 		os.remove(imageFile)
 
-		# Give emtpty string back
+		# Return emtpty string
 		im = ''
 
 		# Return it failed
@@ -50,12 +56,16 @@ def main():
 	height = 400 
 	##########################################
 
-	# Name of directory where processed images are stored:
+	# Path to where processed images are stored:
 	procImgs = './images/processed'	
+
+	# Path to the bounding box annotations
+	xmlDir = './images/annotations'
 
 	# Try to open file, if error delete
 	for inFile in os.listdir(procImgs):
 		
+		# Path to processed image files
 		filePath = os.path.join(procImgs, inFile)
 
 		# Check if we can open the image:
@@ -68,7 +78,29 @@ def main():
 				im.close()
 				os.remove(filePath)
 
+		# Let's check the associated .xml file is also there
+		xmlFile = os.path.split(inFile)[-1].split('.')[0] + '.xml'
+
+		# Path to .xml file with bounding box annotations
+		xmlPath = os.path.join(xmlDir, xmlFile)
+
+		# If not, store the name of the image file in a .txt file
+		if not os.path.isfile(xmlPath):
+
+			# Open file and write name of .jpg with missing annotated file
+			missingTxt = open('missingAnnotations.txt', 'a')
+			missingTxt.write(inFile + '\n')
+			missingTxt.close()
+
+
 	print("Check Complete.")
+
+	# Check if any files where missing.
+	isEmpty = os.stat('missingAnnotations.txt').st_size == 0
+	if not isEmpty:
+		print('Missing .xml annotation for .jpg file(s).\n')
+		print('Please refer to "missingAnnotations.txt" file to view .jpg files which are missing annotations.')
+
 
 if __name__ == '__main__':
 	main()
--- CakeDetect-YOLO/02_split_dataset.py
+++ CakeDetect-YOLO/02_split_dataset.py
@@ -5,7 +5,8 @@
 # Description:
 # ------------
 # Splits images and their associated annotations into a training and 
-# validation set.
+# validation set. Creates and stores images and their associated 
+# annotations into separate folders (four in total).
 #
 # Returns:
 # --------
@@ -36,6 +37,9 @@ def createTrainValDir(imageDirectory:str):
 	"""
 	Create directories to store the train, validation, and annotated
 	image files.
+
+	Parameters:
+	---------
 	"""
 	directoryNames = ['train', 'valid', 'train_annot', 'valid_annot']
 
@@ -50,16 +54,74 @@ def createTrainValDir(imageDirectory:str):
 
 def copyMoveFiles(fileNames:str, inputDirectory:str, outputDirectory:str):
 	"""
-	Copy files from one directory into another
+	Copy image files from one directory into another.
+
+	Parameters:
+	---------
 	"""
 
 	for file in fileNames:
+		
+		# input directory
 		source = os.path.join(inputDirectory, file)
-		dst = os.path.join(outputDirectory, file)
+
+		# Where to store the files
+		dst = os.path.join(outputDirectory, file) 
+		
+		# Copy image to distination folder.
 		copyfile(source, dst)
 
 
-def main()
+def copyMoveAnnotations(fileNames:str, inputDirectory:str, outputDirectory:str):
+	"""
+	Copy annotation files from one directory to another.
+
+	Parameters:
+	---------
+	"""
+
+	for file in fileNames:
+
+		# First, grab file number
+		annotPrefix = file[:-4]
+		
+		# Paste with correct output file name
+		annot_file = annotPrefix + '.xml'
+
+		source = os.path.join(inputDirectory, annot_file)
+
+		# Copy image to destination folder.
+		if os.path.split(outputDirectory)[-1] == 'valid_annot':
+			trueOutputDir = './images/valid_annot/'
+
+			# Path to file with annotations for validation images
+			dst = os.path.join(trueOutputDir, annot_file)
+
+			# Copy annotation to separate folder
+			copyfile(source, dst)
+		else:
+			trueOutputDir = './images/train_annot/'
+
+			# Path to file with annotations for training images
+			dst = os.path.join(trueOutputDir, annot_file)
+
+			# Copy annotation to separate folder
+			copyfile(source, dst)
+		
+
+
+# Create the parser
+argparser = argparse.ArgumentParser(
+	description='Split data set into separate directories.')
+
+# Add arguments
+argparser.add_argument(
+	'path',
+	 type = str,
+	 help ='path to boundary box annotations')
+
+
+def main(args):
 	############################################
 	# TO DO
 	# store name of image file in config file?
@@ -70,6 +132,10 @@ def main()
 
 	# Name of directory where processed images are stored:
 	procImgs = './images/processed'
+
+	# Name of directory where the annotations are:
+	annotDirectory = args.path
+	#annotDirectory = './images/annotations/'
 
 	# Get file names and make sure it is a file and not a directory
 	fileNames = [name for name in os.listdir(procImgs) if os.path.isfile(os.path.join(procImgs, name))]
@@ -96,16 +162,21 @@ def main()
 	# Create directory(s) to store train, val, and annotation files
 	directoryPaths = createTrainValDir(imageDir)
 
-	# Read in 'x' number of files int train, validation folders
+	print(directoryPaths)
+
+	# Read in 'x' number of files into train, validation folders
 	copyMoveFiles(trainFileNames, procImgs, directoryPaths[0])
 	copyMoveFiles(validFileNames, procImgs, directoryPaths[1])
 
-	print("Copying complete")
+	# Read in 'x' number of files into train and validation annotation folders
+	copyMoveAnnotations(trainFileNames, annotDirectory, directoryPaths[2])
+	copyMoveAnnotations(validFileNames, annotDirectory, directoryPaths[3])
 
-	#########################################################################
-	# TO DO Read in 'x' number of files into train_annot, valid_annot folders
+	print("Copying complete")
 
 
 if __name__ == '__main__':
-	main()
 
+	# Exectute parse_args()
+	args = argparser.parse_args()
+	main(args)
