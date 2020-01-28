6b5b2b5a90301f245d09d0005e85aaedfa44a2d9
Luna <bluna@berkeley.edu>
2020-01-28 11:57:34 -0800

    Added backend.py and frontend.py, began train.py script

--- CakeDetect-YOLO/03_train.py
+++ CakeDetect-YOLO/03_train.py
@@ -1,235 +1,89 @@
 #!/bin/usr/python3
 # -------------------------------------------------------------------
-# Author: Noah G. Luna 
+# Author: experiencor/keras-yolo2 with  modification by Noah G. Luna
 #
 # Description:
 # ------------
-#
+# 
 # -------------------------------------------------------------------
 
+
 import os
+import argparse
 import numpy as np
-import tensorflow as tf
-from keras.models import Model
-from keras.layers import Input, Reshape, Lambda
+from preprocessing import parse_annotation
+from frontend import YOLO 
+import json
+
 
 
 ## TO DO ##
 
+# Create config file
 # See if I am using weights from ImageNet or the onedefined on the GitHub one
 # Get tiny yolo weights
 # Understand the BaseFeatureExtractor Class
+# Understand loss function in YOLO paper.
+# Read YOLO paper
 
+# Make command line-friendly
+argparser = argparse.ArgumentParser(description="Train and validate YOLO_V2")
 
+argparser.add_argument('-c','--conf',help='path to configuration file')
 
 
-TINY_YOLO_BACKEND_PATH = "tiny_yolo_backend.h5"
-#MOBILENET_BACKEND_PATH = "mobilenet_backend.h5"
+def main(args):
+	config_path = args.conf 
 
+	with open(config_path) as config_buffer:
+		conf = json.loads(config_buffer.read())
 
 
+	###############################
+	#   Parse the annotations 
+	###############################
 
-class BaseFeatureExtractor(object):
-	"""
-	Checks,,,,
-	"""
 
-	# Will be defined in each subclass
-	def __init__(self, input_size):
-		raise NotImplementedError("error message")
+	###############################
+	#   Construct the model
+	###############################
 
-	# Will be defined in each subclass
-	def normalize(self, image):
-		raise NotImplementedError("error message")
+	# Define yolo instance from YOLO Class
+	yolo = YOLO(backend 			= config['model']['backend'],
+				input_size 			= config['model']['input_size'],
+				labels 				= config['model']['labels'],
+				max_box_per_image 	= config['model']['max_box_per_image'],
+				anchors 			= config['model']['anchors'])
 
-	def get_output_shape(self):
-		return self.feature_extractor.get_output_shape_at(-1)[1:3]
 
-	def extract(self, input_image):
-		return self.feature_extractor(input_image)
+	###############################
+	#   Load the pretrained weights (if any)
+	###############################
 
 
 
-class TinyYoloFeature(BaseFeatureExtractor):
-	"""
-	Defines Tiny YOLO architecture for feature extracting.
-	"""
+	###############################
+	#   Start Training
+	###############################
 
-	def __init__(self, input_size):
-		input_image = Input(shape=(input_size, input_size), 3)
+    yolo.train(train_imgs  = train_imgs,
+		valid_imgs         = valid_imgs,
+		train_times        = config['train']['train_times'],
+		valid_times        = config['valid']['valid_times'],
+		nb_epochs          = config['train']['nb_epochs'], 
+		learning_rate      = config['train']['learning_rate'], 
+		batch_size         = config['train']['batch_size'],
+		warmup_epochs      = config['train']['warmup_epochs'],
+		object_scale       = config['train']['object_scale'],
+		no_object_scale    = config['train']['no_object_scale'],
+		coord_scale        = config['train']['coord_scale'],
+		class_scale        = config['train']['class_scale'],
+		saved_weights_name = config['train']['saved_weights_name'],
+		debug              = config['train']['debug'])
 
-		# Layer 1
-		x = Conv2D(filters=16, kernel_size=(3,3), strides=(1,1),\
-			padding='same', name='conv_1', use_bias=False)(input_image)
-		x = BatchNormalization(name='norm_1')(x)
-		x = LeakyReLU(alpha=0.1)(x)
-		x = MaxPooling2D(pool_size=(2,2))(x)
 
-		# Layer 2 - 5
-        for i in range(0,4):
-			x = Conv2D(32*(2**i), (3,3), strides=(1,1), padding='same',\
-				name='conv_' + str(i+2), use_bias=False)(x)
-			x = BatchNormalization(name='norm_' + str(i+2))(x)
-			x = LeakyReLU(alpha=0.1)(x)
-			x = MaxPooling2D(pool_size=(2, 2))(x)
 
-		# Layer 6
-		x = Conv2D(filters=512, kernel_size=(3,3), strides=(1,1),\
-			padding='same', name='conv_6', use_bias=False)(x)
-		x = BatchNormalization(name='norm_6')(x)
-		x = LeakyReLU(alpha=0.1)(x)
-		x = MaxPooling2D(pool_size=(2, 2), strides=(1,1), padding='same')(x)
-
-		# Layers 7 - 8
-        for i in range(0,2):
-			x = Conv2D(1024, (3,3), strides=(1,1), padding='same',\
-				name='conv_' + str(i+7), use_bias=False)(x)
-			x = BatchNormalization(name='norm_' + str(i+7))(x)
-			x = LeakyReLU(alpha=0.1)(x)
-
-		self.feature_extractor = Model(input_image, x)
-		self.feature_extractor.load_weights(TINY_YOLO_BACKEND_PATH)
-
-	def normalize(self, image):
-		return image / 255.
-
-
-
-class MobileNetFeature(BaseFeatureExtractor):
-	"""
-	Defines MobileNet architecture for feature extracting. In this
-	model we are using the MobileNetV2 with weights trained on the 
-	ImageNet data set.
-	"""
-	def __init__(self, input_size):
-		input_image = Input(shape=(input_size, input_size, 3))
-
-		mobilenet = MobileNetV2(input_shape=(224, 224, 3),\
-			include_top = False, weights = 'imagenet')
-		# mobilenet.load_weights(MOBILENET_BACKEND_PATH)
-
-		x = mobilenet(input_image)
-
-		# Define instance feature extractor model and set
-		# as attribue of class
-		self.feature_extractor = Model(input_image, x)
-
-
-    def normalize(self, image):
-		image = image / 255.
-		image = image - 0.5
-		image = image * 2.
-
-		return image
-
-
-
-class YOLO(object):
-	"""
-	DocString for Class
-	"""
-	def __init__(self, backend,
-		input_size,
-		labels,
-		max_box_per_image,
-		anchors)
-
-	self.input_size = input_size
-	self.labels = labels
-	self.nb_class = len(self.labels)	
-	self.nb_box = len(anchors)
-	self.anchors = anchors
-	self.class_wt = np.ones(self.nb_class, dtype = 'float32')
-	self.max_box_per_image = max_box_per_image
-
-
-	############################################
-	# Make the model
-	############################################
-
-
-	# Make the feature extractor layers
-	input_image = Input(shape=(self.input_size, self.input_size, 3))
-	self.true_boxes = Input(shape=(1, 1, 1, max_box_per_image, 4))
-
-
-	# Set the backend. This is our feature extractor/base model
-	if backend == 'MobileNet':
-		self.feature_extractor = MobileNetFeature(self.input_size)
-	elif backend == 'Tiny Yolo':
-		self.feature_extractor = TinyYoloFeature(self.input_size)
-	else:
-		raise Exception('Architecture is not supported. Please see list of supported architectures.')
-
-
-	# Print the feature extractor shape as defined in the Class specified above
-	print(self.feature_extractor.get_output_shape())
-
-	# Assign feature extractor shape to instance variabkes so we can use this as input
-	# to our YOLO object detector
-	self.grid_h, self.grid_w = self.feature_extractor.get_output_shape() 
-	features = self.feature_extractor.extract(input_image)
-
-
-	####### YOLO #########
-	# Last Layer
-	# Define the object detection layer (the last portion of the model)
-	output = Conv2D(self.nb_box * (4 + 1 + self.nb_class), (1,1), strides=(1,1), 
-		padding='same', name='DetectionLayer', kernel_initializer='lecun_normal')(features)
-	
-	output = Reshape((self.grid_h, self.grid_w, self.nb_box, 4 + 1 + self.nb_class))(output)
-
-	# small hack to allow true_boxes to be registered when Keras build the model 
-	output = Lambda(lambda args: args[0])([output, self.true_boxes])
-
-	# Define the entire model, then get weights of the last layer
-	self.model = Model([input_image, self.true_boxes], output)
-
-	# Get last convolutional layer and its old weights
-	layer = self.model.layers[-4]
-	weights = layer.get_weights()
-
-	# Randomize the weights of the last layer, use dimension of weights to define shape needed
-	new_kernel = np.random.normal(size=weights[0].shape)/(self.grid_h*self.grid_w)
-	new_bias   = np.random.normal(size=weights[1].shape)/(self.grid_h*self.grid_w)
-
-	# Assign the new and randomized weights to the last layer
-	layer.set_weights([new_kernel, new_bias])
-
-	# print summary of the whole model
-	self.model.summary()
-
-	
-	# Define a custom loss function
-
-	# Define the train function
-
-	# Define evaluate
-
-
-
-
-
-
-def main():
-	
-
-	# Load data in (in a memory efficient way)
-
-	# rescale images
-
-	# Create the base model from the pre-trained model MobileNet V2
-	yolo = YOLO(backend = '',\
-		input_size = '',\
-		labels = '',\
-		max_box_per_image = '',\
-		anchors = '')
-
-	# Compile model
-
-	# Train model
-
-	# Plot learning curves
 
 if __name__ == '__main__':
-	main()
+	args = argparser.parse_args()
+	main(args)
---
+++ CakeDetect-YOLO/backend.py
@@ -0,0 +1,153 @@
+#!/bin/usr/python3
+# -------------------------------------------------------------------
+# Author: experiencor/keras-yolo2 with modification by NOah G. Luna
+#
+# Description:
+# ------------
+# Implementation of architectures defined by: experiencor/keras-yolo2
+#
+# Modifications:
+# --------------
+# Original script modified s.t.:
+# Only have 'smaller' architectures were kept: tinyYOLO, MobileNet and ResNet
+# Added MobileNetV2 which downloads weights from server directly.
+# -------------------------------------------------------------------
+
+
+from keras.models import Model
+import tensorflow as tf
+from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Lambda
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.merge import concatenate
+from keras.applications.mobilenet import MobileNet
+from keras.applications import InceptionV3
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
+
+
+MOBILENET_BACKEND_PATH  = "mobilenet_backend.h5"   # should be hosted on a server
+TINY_YOLO_BACKEND_PATH  = "tiny_yolo_backend.h5"   # should be hosted on a server
+RESNET50_BACKEND_PATH   = "resnet50_backend.h5"    # should be hosted on a server
+
+
+class BaseFeatureExtractor(object):
+    """docstring for ClassName"""
+
+    # to be defined in each subclass
+    def __init__(self, input_size):
+        raise NotImplementedError("error message")
+
+    # to be defined in each subclass
+    def normalize(self, image):
+        raise NotImplementedError("error message")       
+
+    def get_output_shape(self):
+        return self.feature_extractor.get_output_shape_at(-1)[1:3]
+
+    def extract(self, input_image):
+        return self.feature_extractor(input_image)
+
+class MobileNetFeature(BaseFeatureExtractor):
+    """docstring for ClassName"""
+    def __init__(self, input_size):
+        input_image = Input(shape=(input_size, input_size, 3))
+
+        mobilenet = MobileNet(input_shape=(224,224,3), include_top=False)
+        mobilenet.load_weights(MOBILENET_BACKEND_PATH)
+
+        x = mobilenet(input_image)
+
+        self.feature_extractor = Model(input_image, x)  
+
+    def normalize(self, image):
+        image = image / 255.
+        image = image - 0.5
+        image = image * 2.
+
+        return image
+
+
+class MobileNetFeatureV2(BaseFeatureExtractor):
+    """
+    Defines MobileNet architecture for feature extracting. In this
+    model we are using the MobileNetV2 with weights trained on the 
+    ImageNet data set.
+    """
+    def __init__(self, input_size):
+        input_image = Input(shape=(input_size, input_size, 3))
+
+        mobilenet = MobileNetV2(input_shape=(224, 224, 3),\
+            include_top = False, weights = 'imagenet')
+
+        x = mobilenet(input_image)
+
+        # Define instance feature extractor model and set
+        # as attribue of class
+        self.feature_extractor = Model(input_image, x)
+
+
+    def normalize(self, image):
+        image = image / 255.
+        image = image - 0.5
+        image = image * 2.
+
+        return image
+
+
+
+class TinyYoloFeature(BaseFeatureExtractor):
+    """docstring for ClassName"""
+    def __init__(self, input_size):
+        input_image = Input(shape=(input_size, input_size, 3))
+
+        # Layer 1
+        x = Conv2D(16, (3,3), strides=(1,1), padding='same', name='conv_1', use_bias=False)(input_image)
+        x = BatchNormalization(name='norm_1')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+
+        # Layer 2 - 5
+        for i in range(0,4):
+            x = Conv2D(32*(2**i), (3,3), strides=(1,1), padding='same', name='conv_' + str(i+2), use_bias=False)(x)
+            x = BatchNormalization(name='norm_' + str(i+2))(x)
+            x = LeakyReLU(alpha=0.1)(x)
+            x = MaxPooling2D(pool_size=(2, 2))(x)
+
+        # Layer 6
+        x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_6', use_bias=False)(x)
+        x = BatchNormalization(name='norm_6')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), strides=(1,1), padding='same')(x)
+
+        # Layer 7 - 8
+        for i in range(0,2):
+            x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_' + str(i+7), use_bias=False)(x)
+            x = BatchNormalization(name='norm_' + str(i+7))(x)
+            x = LeakyReLU(alpha=0.1)(x)
+
+        self.feature_extractor = Model(input_image, x)  
+        self.feature_extractor.load_weights(TINY_YOLO_BACKEND_PATH)
+
+    def normalize(self, image):
+        return image / 255.
+
+
+
+class ResNet50Feature(BaseFeatureExtractor):
+    """docstring for ClassName"""
+    def __init__(self, input_size):
+        resnet50 = ResNet50(input_shape=(input_size, input_size, 3), include_top=False)
+        resnet50.layers.pop() # remove the average pooling layer
+        #resnet50.load_weights(RESNET50_BACKEND_PATH)
+
+        self.feature_extractor = Model(resnet50.layers[0].input, resnet50.layers[-1].output)
+
+    def normalize(self, image):
+        image = image[..., ::-1]
+        image = image.astype('float')
+
+        image[..., 0] -= 103.939
+        image[..., 1] -= 116.779
+        image[..., 2] -= 123.68
+
+        return image 
