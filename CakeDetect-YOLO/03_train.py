ece07ddfcddbae7d1abda0140892cb2cb9b7de89
Luna <bluna@berkeley.edu>
2020-02-11 21:20:25 -0800

    updated train.py and removed tmp imgAug.py

--- CakeDetect-YOLO/03_train.py
+++ CakeDetect-YOLO/03_train.py
@@ -4,7 +4,7 @@
 #
 # Description:
 # ------------
-# 
+# YOLOv2 with MobileNet backend work-in-progress.
 # -------------------------------------------------------------------
 import os
 import argparse
@@ -13,14 +13,7 @@ from processing import parse_annotation
 from frontend import YOLO 
 import json
 
-## TO DO ##
-
-# Fix Issue:
-# AttributeError: module 'tensorflow' has no attribute 'assign_add'
-
-# Find out about anchors (see line 37)
-
-# Understand the BaseFeatureExtractor Class
+####### TO DO #########
 
 # Understand loss function in YOLO paper./Read Paper
 
@@ -29,96 +22,72 @@ import json
 # https://github.com/experiencor
 # https://github.com/makatx/YOLO_ResNet/blob/master/model_continue_train.py
 
-def main():
 
-	###############################
-	#   Define Parameters of Model
-	architecture 		= "MobileNetFeatureV2"  # "Tiny Yolo" or "MobileNet" or "ResNet"
-	input_size 			= 400
-	anchors 			= [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
-	max_box_per_image 	= 5
-	labels 				= ['cake']	
+####### THINGS TO UPDATE ON ORIGINAL #######
+#tf.Print -> tf.print
 
-	# Define parameters of Training Data
-	train_image_folder = './images/train/'
-	train_annot_folder = './images/train_annot/'
+#seen = tf.assign_add(seen, 1.) # depecrated TensorFlow v1
+#seen.assign_add(1.0)
+##############
 
-	train_times		 	= 8 # the number of time to cycle through the training set, useful for small datasets
-	pretrained_weights 	= '' # path of the pretrained weights, OK to start from scratch
-	batch_size 			= 16 # number of images to read in each batch
-	learning_rate		= 1e-4 # the base learning rate of the default Adam rate scheduler
-	nb_epochs 			= 50 # number of epochs
-	warmup_epochs 		= 2 # number of initial epochs during which the size of the 5 boxes in each cell is forced to match the sizes of the 5 anchors, this trickseems to improve emperically
 
-	object_scale 		= 5.0 # determine how much to penalize wrong prediction of confidence of object predictors
-	no_object_scale 	= 1.0 # determine how much to penalize wrong prediction of confidence of non-object predictors
-	coord_scale			= 1.0 # determine how much to penalize wrong position and size predictions (x, y, w, h)
-	class_scale			= 1.0 # determine how much to penalize wrong class prediction
+def main(args):
+	config = args.config
 
-	saved_weights_name 	= 'best_weights.h5' # name of the output file
-	debug				= True
-
-	# Define parameters of the validation data
-	valid_image_folder 	= './images/valid/'
-	valid_annot_folder 	= './images/valid_annot/'
-	valid_times 		= 1
-
-	###############################
-	#   Parse the annotations 
-	###############################
-
+	
 	# parse annotations of the training set
-	train_imgs, train_labels = parse_annotation(ann_dir = train_annot_folder, 
-		img_dir = train_image_folder, 
-		labels = labels)
+	train_imgs, train_labels = parse_annotation(ann_dir = config['train']['train_annot_folder'], 
+		img_dir = config['train']['train_image_folder'], 
+		labels = config['model']['labels'])
 
 	# parse annotations of the validation set
-	valid_imgs, valid_labels = parse_annotation(ann_dir = valid_annot_folder, 
-		img_dir = valid_image_folder, 
-		labels = labels)    
+	valid_imgs, valid_labels = parse_annotation(ann_dir = config['valid']['valid_annot_folder'], 
+		img_dir = config['valid']['valid_image_folder'], 
+		labels = config['model']['labels'])    
 
 	###############################
 	#   Construct the model
 	###############################
 
 	# Define yolo instance from YOLO Class
-	yolo = YOLO(backend 			= architecture,
-				input_size 			= input_size,
-				labels 				= labels,
-				max_box_per_image 	= max_box_per_image,
-				anchors 			= anchors)
+    yolo = YOLO(backend         = config['model']['backend'],
+			input_size          = config['model']['input_size'], 
+			labels              = config['model']['labels'], 
+			max_box_per_image   = config['model']['max_box_per_image'],
+			anchors             = config['model']['anchors'])
 
 
 	###############################
 	#   Load the pretrained weights (if any)
 	###############################
 
-	if os.path.exists(pretrained_weights):
-		print("Loading pre-trained weights in", pretrained_weights)
-		yolo.load_weights(pretrained_weights)
+    if os.path.exists(config['train']['pretrained_weights']):
+		print("Loading pre-trained weights in", config['train']['pretrained_weights'])
+		yolo.load_weights(config['train']['pretrained_weights'])
 
 
 	###############################
 	#   Start Training
 	###############################
 
-	yolo.train(train_imgs  = train_imgs,
-		valid_imgs         = valid_imgs,
-		train_times        = train_times,
-		valid_times        = valid_times,
-		nb_epochs          = nb_epochs,
-		learning_rate      = learning_rate,
-		batch_size         = batch_size,
-		warmup_epochs      = warmup_epochs,
-		object_scale       = object_scale,
-		no_object_scale    = no_object_scale,
-		coord_scale        = coord_scale,
-		class_scale        = class_scale,
-		saved_weights_name = saved_weights_name,
-		debug              = debug)
+	yolo.train(train_imgs         = train_imgs,
+				valid_imgs         = valid_imgs,
+				train_times        = config['train']['train_times'],
+				valid_times        = config['valid']['valid_times'],
+				nb_epochs          = config['train']['nb_epochs'], 
+				learning_rate      = config['train']['learning_rate'], 
+				batch_size         = config['train']['batch_size'],
+				warmup_epochs      = config['train']['warmup_epochs'],
+				object_scale       = config['train']['object_scale'],
+				no_object_scale    = config['train']['no_object_scale'],
+				coord_scale        = config['train']['coord_scale'],
+				class_scale        = config['train']['class_scale'],
+				saved_weights_name = config['train']['saved_weights_name'],
+				debug              = config['train']['debug'])
 
 
 
 
 if __name__ == '__main__':
-	main()
+	args = argparser.parse_args()
+	main(args)
